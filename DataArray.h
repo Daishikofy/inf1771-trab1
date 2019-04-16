3539c19ba8f8c90ea803787af2a4cea5ff0f7ee3
Daishikofy <daishi.setsuko@gmail.com>
2019-04-16 19:11:11 -0300

    Comentario das funcoes

--- DataArray.h
+++ DataArray.h
@@ -12,7 +12,15 @@ struct Client
 class DataArray 
 {
 public:
+
+	/**
+	*	clients: vetor de clients, o primeiro client eh  deposito. 
+	*	capacity: capacidade de transporte de um caminhao
+	*	nTrucks: numero de rotas 
+	*   filePath: caminho ate o txt contendo os dados. A formatacao do txt deve ser especifica 
+	**/
 	void ReadFile (std::vector<Client>& clients, int& capacity, int& nTucks, std::string filePath );
+	
 private:
 	Client FillClient (std::string line);
 	void PrintClient (Client client);
